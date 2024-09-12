bb93ef6506985c1c01af9fdf4241e5cc5f18ab66
maksanik <maksimmor2012@yandex.ru>
2024-09-12 13:33:52 +0300

    Added category change by GET.

--- main/views.py
+++ main/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 from goods.models import Category, Product
 
 def index(request):
+    
+    category_slug = request.GET.get("category", None)
+    if category_slug == None:
+         products = Product.objects.all()
+    else:
+        products = Product.objects.filter(category__slug=category_slug).order_by("-id")
+    
     categories = Category.objects.all()
-    products = Product.objects.all().order_by("-id")
+    products = products.order_by("-id")
     
     context = {
         'categories': categories,
